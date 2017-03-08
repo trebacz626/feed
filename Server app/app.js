7fecdc25c76c5331ef0a8921317fdb79ba2e1fae
LenovoFlex3\treba <trebacz.kacper@gmail.com>
2017-03-08 15:52:51 +0100

    Correct

--- Server app/app.js
+++ Server app/app.js
@@ -1,38 +1,21 @@
 var express = require('express');
+var Session= require('express-session');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var mysql = require('mysql');
+var google = require('googleapis');
+var OAuth2 = google.auth.OAuth2;
+var plus = google.plus('v1');
 
-var session = require("express-session");
-var mysql=require('mysql');
-var connection=mysql.createConnection({
-  host:"localhost",
-  user:"root",
-  password:"root",
-  database:"simplecookingdb"
-});
-
-connection.connect();
-var passport = require('passport');
-
-var flash=require('connect-flash');
-
-
-require('./config/passport')(passport,connection);
+var googleDATA=require('./config/google');
 //var index = require('./routes/index');
 //var users = require('./routes/users');
 
 var app = express();
-app.use(flash());
-/*app.use(connection(mysql,{
-	host:"localhost",
-	user:"root",
-	password:"root",
-	database:"test"
-},'request'));
-*/
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
@@ -44,15 +27,71 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({secret:"anystringText",
-saveUninitialized:true,
-resave:true}));
-app.use(passport.initialize());
-app.use(passport.session());
+app.use(Session({
+    secret: 'your-random-secret-19890913007',
+    resave: true,
+    saveUninitialized: true
+}));
+
+function getOAuthClient () {
+    return new OAuth2(googleDATA.ClientId ,  googleDATA.ClientSecret, googleDATA.RedirectionUrl);
+}
+ 
+function getAuthUrl () {
+    var oauth2Client = getOAuthClient();
+    // generate a url that asks permissions for Google+ and Google Calendar scopes
+    var scopes = [
+      'https://www.googleapis.com/auth/plus.me'
+    ];
+ 
+    var url = oauth2Client.generateAuthUrl({
+        access_type: 'offline',
+        scope: scopes // If you only need one scope you can pass it as string
+    });
+ 
+    return url;
+}
+
+app.use("/auth/google/callback", function (req, res) {
+    var oauth2Client = getOAuthClient();
+    var session = req.session;
+    var code = req.query.code;
+    oauth2Client.getToken(code, function(err, tokens) {
+      // Now tokens contains an access_token and an optional refresh_token. Save them.
+      if(!err) {
+        oauth2Client.setCredentials(tokens);
+        session["tokens"]=tokens;
+        res.render('loginSuccess',{url:'/details'});
+      }
+      else{
+        res.render('loginFailed',{error:err});
+      }
+    });
+});
+ 
+app.use("/details", function (req, res) {
+    var oauth2Client = getOAuthClient();
+    oauth2Client.setCredentials(req.session["tokens"]);
+ 
+    var p = new Promise(function (resolve, reject) {
+        plus.people.get({ userId: 'me', auth: oauth2Client }, function(err, response) {
+            resolve(response || err);
+        });
+    }).then(function (data) {
+	console.log(data);
+        res.render('details',{datas:data});
+    })
+});
+ 
+app.use("/", function (req, res) {
+    var url = getAuthUrl();
+console.log(url);
+    res.render("index",{url:url})
+});
 
 //app.use('/', index);
 //app.use('/users', users);
-var routes = require('./routes/routes')(app,passport,connection);
+//require('./routes/routes')(app,connection);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
